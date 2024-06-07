32a49474ba2dfe0205befe14b8ce78b5bbbe2656
ManishMadan2882 <manishmadan321@gmail.com>
2024-06-08 04:15:33 +0530

    fixed: internal server

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,11 @@ import dotenv from "dotenv"
 import axios from "axios"
 import moment from "moment";
 import cors from 'cors'
+import path from "path"
 dotenv.config();
-
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 3000;
 const GITHUB_ORG = process.env.GITHUB_ORG;
